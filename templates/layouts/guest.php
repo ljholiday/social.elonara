5aa8f1d79985d59344e9e3009f05791e7269c863
ljholiday <lonn@ljholiday.com>
2026-02-25 10:12:08 -0800

    Revert "Restore scrolling on guest pages"
    This reverts commit 1d378a4fcdff82d6c4160c34f5816efcdd8da7f7.

--- templates/layouts/guest.php
+++ templates/layouts/guest.php
@@ -22,7 +22,7 @@ $fullTitle = $page_title === $appName ? $appName : $page_title . ' - ' . $appNam
 $security = app_service('security.service');
 $csrf_token = $security->createNonce('app_nonce');
 ?><!DOCTYPE html>
-<html lang="en" class="app-page-form">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -35,7 +35,7 @@ $csrf_token = $security->createNonce('app_nonce');
     <link rel="manifest" href="/manifest.json">
     <link rel="stylesheet" href="<?= htmlspecialchars($assetBase . '/css/app.css', ENT_QUOTES, 'UTF-8'); ?>">
 </head>
-<body class="app-page-form">
+<body >
 
 <div >
     <main class="app-main" role="main">
