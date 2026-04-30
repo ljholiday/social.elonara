a8921422c479f5d07dea7fa5391d1ec6ab281b51
Lonn Holiday <lonn@ljholiday.com>
2026-04-30 11:35:47 -0700

    Allow World to frame Social

--- src/Services/SecurityService.php
+++ src/Services/SecurityService.php
@@ -226,10 +226,9 @@ final class SecurityService
     {
         return [
             'X-Content-Type-Options' => 'nosniff',
-            'X-Frame-Options' => 'DENY',
             'X-XSS-Protection' => '1; mode=block',
             'Referrer-Policy' => 'strict-origin-when-cross-origin',
-            'Content-Security-Policy' => "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+            'Content-Security-Policy' => "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-ancestors 'self' https://world.elonn.com",
             'Strict-Transport-Security' => 'max-age=31536000; includeSubDomains'
         ];
     }
